c3c111f400ceec6d0ea93ab4a7fd4b85cf7cc1c0
shorfng <shorfng@126.com>
2016-11-17 17:20:03 +0800

    版本：0.1 Works：创建项目，并初始化

---
+++ Bettalive/Bettalive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bettalive
+//
+//  Created by 蓝田 on 2016/11/17.
+//  Copyright © 2016年 蓝田. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
